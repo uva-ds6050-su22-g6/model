df149a90d24b892dce2afb55df2a66166385c501
Robert Knuuti <uqq5zz@virginia.edu>
2022-07-16 11:56:27 -0400

    Added ignore for data folder and disabled autosave in ipynb.

--- model.py
+++ model.py
@@ -1,4 +1,20 @@
-#!/usr/bin/env python
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% Disable autosave
+# %autosave 0
 
 # %% Data setup
 import numpy as np
